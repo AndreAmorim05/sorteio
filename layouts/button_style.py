34e74eb3eef4c0add3d4227707fe1d500f78181d
André Amorim <js.andreamorim@gmail.com>
2019-06-16 10:27:44 -0300

    Alteração do button_style para apresentar as imagens no botão, sem êxito

--- layouts/button_style.py
+++ layouts/button_style.py
@@ -4,13 +4,20 @@ from kivy.uix.label import Label
 from kivy.utils import get_color_from_hex
 from kivy.graphics import Color, Ellipse, Rectangle
 from kivy.properties import StringProperty
+from kivy.graphics.texture import Texture
+from kivy.core.image import Image as CoreImage
 
-class ButtonStyle(ButtonBehavior, Label):
+
+class ButtonStyle(ButtonBehavior, Label, Image):
     source_image = StringProperty('')
     def __init__(self, **kwargs):
         super(ButtonStyle, self).__init__(**kwargs)
+        # self.tex = Texture.create(size=(137, 80), colorfmt='rgba')
         self.atualizar()
         # self.background_down = ''
+
+    def on_source_image(self, *args):
+        self.atualizar()
 
     def on_pos(self, *args):
         self.atualizar()
@@ -28,10 +35,15 @@ class ButtonStyle(ButtonBehavior, Label):
                     pos=(self.x + self.width - self.height, self.y))
             Rectangle(size=(self.width - self.height, self.height),
                       pos=(self.x + self.height/2.0, self.y))
+            Rectangle(pos=(self.x + self.height/4, self.y + self.height * 0.3),
+                  size=(self.height * 0.4, self.height * 0.4),
+                  texture=self.tex)
+
             Image(pos=(self.x + self.height/4, self.y + self.height * 0.3),
                   source=self.source_image,
                   size=(self.height * 0.4, self.height * 0.4))
+            # ,
+            # texture=self.tex
 
-    def load_image(self):
-        pass
+# source=self.source_image
 # cor do ícone do botão F5F6F2
